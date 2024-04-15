257062b3bea78a7edd1b2eb6c2df6da3e319c43d
Will Fisher <whfisher@alaska.edu>
2024-04-15 12:29:20 -0800

    Add support for databag and credentials
    Signed-off-by: Will Fisher <whfisher@alaska.edu>

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,7 +2,7 @@ default['golang']['version'] = '1.22.1'
 
 default['gcp']['service_account_json'] = '/etc/caddy/gcp_service_account.json'
 default['gcp']['service_account_vault'] = 'gcp_service_account_json'
-default['gcp']['project'] = 'clound-dns-321021'
+default['gcp']['project'] = ''
 
 default['chef-vault']['databag_fallback'] = true
 
@@ -10,10 +10,4 @@ default['caddy']['user'] = 'caddy'
 default['caddy']['group'] = 'caddy'
 default['caddy']['wildcard_domain'] = '*.lab.acep.uaf.edu'
 default['caddy']['acme_email'] = 'uaf-acep-ci@alaska.edu'
-
-default['acep']['proxy_sites'] = [{
-    name: "test",
-    fqdn: "test.camio.lab.acep.uaf.edu",
-    upstream: "http://localhost:8080",
-    self_signed: false
-}]
+default['caddy']['sites_data_bag'] = ''
--- recipes/default.rb
+++ recipes/default.rb
@@ -8,12 +8,11 @@ if ubuntu_platform?
     include_recipe 'acep-caddy::ubuntu'
 end
 
-
-
 service 'caddy' do 
     action :enable
 end
 
+caddy_sites = data_bag_item('caddy', node['caddy']['sites_data_bag'])[:sites]
 gcp_json = chef_vault_item('credentials', node['gcp']['service_account_vault'])
 
 file node['gcp']['service_account_json'] do
@@ -34,7 +33,7 @@ template '/etc/caddy/Caddyfile' do
         wildcard_domain: node['caddy']['wildcard_domain'],
         gcp_project: node['gcp']['project'],
         gcp_service_account_file: node['gcp']['service_account_json'], 
-        sites: node['acep']['proxy_sites']
+        sites: caddy_sites
     action :create
     notifies :run, 'execute[caddy_fmt]', :immediately
     notifies :restart, 'service[caddy]', :delayed
